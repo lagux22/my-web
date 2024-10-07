19bf9061e6a35e6a5cdd2d3744161e9358bc8858
lagux <namnhtse183876@fpt.edu.vn>
2024-10-08 02:26:52 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+
+    // Gửi thông tin người dùng đến server của bạn hoặc xử lý theo cách bạn muốn
+}
